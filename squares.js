7d4345556980e0d56446ec23f83552ce3dad7916
Bao Nguyen <bao.ng.nguyen3@gmail.com>
2019-01-23 21:57:51 -0500

    refactor show and hide all buttons, generate square only generate if there is nothing there

--- squares.js
+++ squares.js
@@ -1,12 +1,14 @@
 document
   .getElementById('GenerateSquares')
   .addEventListener('click', function() {
-    const colors = ['blue', 'green', 'yellow', 'orange'];
-    generateDom(colors);
+    if (document.getElementById('squaresList').children.length === 0) {
+      const colors = ['blue', 'green', 'yellow', 'orange'];
+      generateDom(colors);
+    }
   });
 
 function generateDom(colorsArr) {
-  const squaresDiv = document.getElementById("squaresList");
+  const squaresDiv = document.getElementById('squaresList');
   colorsArr.forEach(color => {
     const square = document.createElement('div');
     square.className = 'squares';
@@ -14,40 +16,42 @@ function generateDom(colorsArr) {
     square.style.display = 'inline-block';
     square.style.margin = '50px';
     square.style.backgroundColor = color;
-    square.style.height = '100px'
+    square.style.height = '100px';
     square.style.width = '100px';
     squaresDiv.appendChild(square);
   });
 
   const commandsList = document.getElementById('commands');
-  for(let i =0; i <=1; ++i){
+
+  ['Show', 'Hide'].forEach(command => {
     const button = document.createElement('button');
-    if(i === 0){
-      button.innerHTML = 'Show All Squares';
-      button.addEventListener('click', function(){
-        const curSquares = document.getElementsByClassName('squares');
-        Array.from(curSquares).forEach(square => square.style.display = 'inline-block');
-      })
-    }else{
-      button.innerHTML= 'Hide All Squares';
-      button.addEventListener('click', function(){
-        const curSquares = document.getElementsByClassName('squares');
-        Array.from(curSquares).forEach(square => square.style.display = 'none');
-      })
-    }
-    commandsList.appendChild(button);
-  }
-  colorsArr.forEach(color =>{
-    const button = document.createElement('button');
-    button.addEventListener('click', function(){
-      const square = document.getElementById(color);
-      if(!square.style.display || square.style.display === 'inline-block'){
-        square.style.display = 'none';
-      }else{
-        square.style.display ='inline-block';
+    button.innerHTML = `${command} All Squares`;
+    button.addEventListener('click', function() {
+      const curSquares = document.getElementsByClassName('squares');
+      if (command === 'Show') {
+        Array.from(curSquares).forEach(
+          square => (square.style.display = 'inline-block')
+        );
+      } else {
+        Array.from(curSquares).forEach(
+          square => (square.style.display = 'none')
+        );
       }
-    })
-    button.innerHTML = `Toggle ${color}`
+    });
     commandsList.appendChild(button);
-  })
+  });
+
+  colorsArr.forEach(color => {
+    const button = document.createElement('button');
+    button.addEventListener('click', function() {
+      const square = document.getElementById(color);
+      if (!square.style.display || square.style.display === 'inline-block') {
+        square.style.display = 'none';
+      } else {
+        square.style.display = 'inline-block';
+      }
+    });
+    button.innerHTML = `Toggle ${color}`;
+    commandsList.appendChild(button);
+  });
 }
