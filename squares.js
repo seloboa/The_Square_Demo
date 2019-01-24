1e9e780558e0cd00f893d0378beeedbcd41a12f4
Bao Nguyen <bao.ng.nguyen3@gmail.com>
2019-01-23 23:26:42 -0500

    refactor generateDom to 1 loop for both buttons and square

--- squares.js
+++ squares.js
@@ -1,29 +1,46 @@
-const generateSquare = document
-  .getElementById('GenerateSquares')
+const generateSquare = document.getElementById('GenerateSquares');
 
 generateSquare.addEventListener('click', function() {
-    const input = document.getElementById('colorInput');
-    if (document.getElementById('squaresList').children.length === 0 && input.value !== '') {
-      const colors = input.value.split(', ');
-      generateDom(colors);
-      input.style.display='none';
-      generateSquare.innerHTML = "Restart The Demo";
-    }else{
-      location.reload();
-    }
-  });
+  const input = document.getElementById('colorInput');
+  if (
+    document.getElementById('squaresList').children.length === 0 &&
+    input.value !== ''
+  ) {
+    const colors = input.value.split(', ');
+    generateDom(colors);
+    input.style.display = 'none';
+    generateSquare.innerHTML = 'Restart The Demo';
+  } else {
+    location.reload();
+  }
+});
 
 function generateDom(colorsArr) {
   const squaresDiv = document.getElementById('squaresList');
+  const buttonsList = document.getElementById('buttonsList');
+
   colorsArr.forEach(color => {
+    console.log('here');
     const square = document.createElement('div');
     square.className = 'squares';
     square.id = color;
     square.style.backgroundColor = color;
-    squaresDiv.appendChild(square);
+    if (square.style.backgroundColor) {
+      squaresDiv.appendChild(square);
+      const button = document.createElement('button');
+      button.addEventListener('click', function() {
+        const square = document.getElementById(color);
+        if (!square.style.display || square.style.display === 'inline-block') {
+          square.style.display = 'none';
+        } else {
+          square.style.display = 'inline-block';
+        }
+      });
+      button.innerHTML = `Toggle ${color}`;
+      button.style.backgroundColor = color;
+      buttonsList.appendChild(button);
+    }
   });
-
-  const buttonsList = document.getElementById('buttonsList');
 
   ['Show', 'Hide'].forEach(command => {
     const button = document.createElement('button');
@@ -40,20 +57,6 @@ function generateDom(colorsArr) {
         );
       }
     });
-    buttonsList.appendChild(button);
-  });
-
-  colorsArr.forEach(color => {
-    const button = document.createElement('button');
-    button.addEventListener('click', function() {
-      const square = document.getElementById(color);
-      if (!square.style.display || square.style.display === 'inline-block') {
-        square.style.display = 'none';
-      } else {
-        square.style.display = 'inline-block';
-      }
-    });
-    button.innerHTML = `Toggle ${color}`;
     buttonsList.appendChild(button);
   });
 }
